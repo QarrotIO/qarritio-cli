8b5aa0eece6d5e130ee3cd6fabbf70d187c9c78c
BenEdgar17 <bencyber100>
2021-09-23 17:05:53 +0300

    Add basic cs files

---
+++ qarrot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace qarrot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to QarrotIO CLI!");
+        }
+    }
+}
