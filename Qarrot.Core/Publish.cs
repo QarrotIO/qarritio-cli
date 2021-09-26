105a7fc12956493a98c8617de43f6f4923fba87a
BenEdgar17 <bencyber100>
2021-09-26 18:34:58 +0300

    Add Core project containing Action interface and Publish classes

---
+++ Qarrot.Core/Publish.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ConsoleAppFramework;
+
+namespace Qarrot.Core
+{
+    [Command("publish", "Execute a Publish action")]
+    public abstract class Publish : ConsoleAppBase, IAction
+    {
+        public virtual void LoadData()
+        {
+            // Load data to be published
+        }
+        
+        public virtual void HandleData()
+        {
+            // Handle data to be published
+        }
+        
+        public virtual void Execute()
+        {
+            Console.WriteLine("Executing Generic Publish...");
+        }
+    }
+}
