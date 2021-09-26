1d68943467e71056dfa052d6602be5254b7edbbd
BenEdgar17 <bencyber100>
2021-09-26 18:35:44 +0300

    Add Base project ought to contain different protocols publishing classes

---
+++ Qarrot.Base/AmqpPublish.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ConsoleAppFramework;
+using Qarrot.Core;
+
+namespace Qarrot.Base
+{
+    public class AmqpPublish : Publish
+    {
+        [Command("amqp", "Publish via AMQP")]
+        public override void Execute()
+        {
+            Console.WriteLine("Publishing via AMQP");
+        }
+    }
+}
