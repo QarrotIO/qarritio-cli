7a4a35f63a075aad3e4c24a2a3dce43bdb518284
BenEdgar17 <bencyber100>
2021-09-28 15:49:52 +0300

    Add Tests project and an example of a test for AmqpPublish command

---
+++ Qarrot.Tests/AmqpPublish.Tests.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using CliFx;
+using CliFx.Infrastructure;
+using NUnit.Framework;
+using Qarrot.Base;
+
+namespace Qarrot.Tests
+{
+    [TestFixture]
+    public class AmqpPublishTests
+    {
+        [Test]
+        public async Task AmqpPublish_Executes_Successfully()
+        {
+            // Arrange
+            using var console = new FakeInMemoryConsole();
+
+            var app = new CliApplicationBuilder()
+                .AddCommand<AmqpPublish>()
+                .UseConsole(console)
+                .Build();
+
+            var args = new[]
+                {"publish", "amqp", "--host", "localhost", "--username", "admin", "--password", "admin"};
+            
+            // Act
+            await app.RunAsync(args);
+
+            var stdout = console.ReadOutputString();
+            
+            // Assert
+            Assert.That(stdout, Is.EqualTo("Publishing via AMQP to: amqp:\\\\admin:admin@localhost:15672...\r\n"));
+        }
+    }
+}
