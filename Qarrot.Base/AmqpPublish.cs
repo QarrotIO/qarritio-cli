5899f7d4d003ae36ce60b6c09fedc4a85175cd5a
BenEdgar17 <bencyber100>
2021-09-28 15:48:48 +0300

    Add properties as options to AmqpPublish command

--- Qarrot.Base/AmqpPublish.cs
+++ Qarrot.Base/AmqpPublish.cs
@@ -8,9 +8,18 @@ namespace Qarrot.Base
     [Command("publish amqp", Description = "Publish via AMQP")]
     public class AmqpPublish : ICommand
     {
+        [CommandOption("host", 'h', Description = "Hostname of the AMQP destination")]
+        public string Hostname { get; set; }
+
+        [CommandOption("username", 'u', Description = "Username of the AMQP destination")]
+        public string Username { get; set; }
+
+        [CommandOption("password", 'p', Description = "Password of the AMQP destination")]
+        public string Password { get; set; }
+
         public ValueTask ExecuteAsync(IConsole console)
         {
-            console.Output.WriteLine("Executing from AmqpPublish..");
+            console.Output.WriteLine($"Publishing via AMQP to: amqp:\\\\{Username}:{Password}@{Hostname}:15672...");
             return default;
         }
     }
